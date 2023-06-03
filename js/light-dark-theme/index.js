f4fa58da3734d9c86564363af1df847ae4632535
jinkazama13200 <jinkazama13200@gmail.com>
2023-06-03 17:02:37 +0700

    added scroll function

--- js/light-dark-theme/index.js
+++ js/light-dark-theme/index.js
@@ -1,4 +1,17 @@
+// Switch theme
 document.getElementById("switchButton").onclick = function () {
   document.getElementById("myMain").classList.toggle("dark");
-
 };
+
+// Scroll Function for Header
+window.onscroll = function () {
+  myFunction();
+};
+
+function myFunction() {
+  if (document.documentElement.scrollTop > 50) {
+    document.getElementById("myHeader").classList.add("scrollTop");
+  } else {
+    document.getElementById("myHeader").classList.remove("scrollTop");
+  }
+}
